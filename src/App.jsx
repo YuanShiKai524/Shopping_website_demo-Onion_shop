4249eb0c5a511b75b7f37c24bce911a9ffaed453
yuanshikai524 <gcobc04992@gmail.com>
2022-11-19 18:01:53 +0800

    刪除舊有src下的data，App組件增加監聽路徑改變以改變網頁title功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { Suspense } from 'react'
-import { useRoutes } from 'react-router-dom'
+import React, { Suspense, useEffect, useState } from 'react'
+import { useLocation, useRoutes } from 'react-router-dom'
+import axios from 'axios'
 import Footer from './components/Footer'
 import Loading from './components/Loading'
 // 引入路由表
@@ -11,6 +12,22 @@ import './App.css'
 const App = () => {
   // 路由表
   const element = useRoutes(routes)
+
+  const [title, setTitle] = useState()
+
+  const location = useLocation()
+
+  // 發送請求取得網頁標題數據
+  axios('/data/titles.json')
+  .then(
+    response => setTitle(response.data),
+    err => console.log(err)
+  )
+
+  useEffect(() => {
+    // 監聽路徑改變，以改變網頁標題
+    document.title = title;
+  }, [location.pathname])
 
   return (
     <div id='app'>
