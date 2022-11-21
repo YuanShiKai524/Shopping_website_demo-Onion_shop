e8330ebb30b86b7fe4cc79ba29535905fdba0cd1
yuanshikai524 <gcobc04992@gmail.com>
2022-11-21 11:03:28 +0800

    把Sidebar組件放到containers文件夾中

--- src/pages/Phone/index.jsx
+++ src/pages/Phone/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../containers/Header'
 import Slider from '../../components/Main/Slider'
-import Sidebar from '../../components/Sidebar'
+import Sidebar from '../../containers/Sidebar'
 import Sort from '../../containers/Sort'
 import ProductList from '../../containers/ProductList'
 
